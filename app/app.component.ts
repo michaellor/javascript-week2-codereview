c82f2a03751d074b9813a5ba7980f76fb459c8a9
michael <michael.vang.1985@gmail.com>
2016-03-25 08:32:57 -0700

    add meal list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -15,11 +15,29 @@ import { Component } from 'angular2/core';
   `
 })
 
+export class MealListComponent {
+  public mealList: Meal[];
+  public onMealSelect: EventEmitter<Meal>;
+  public selectedMeal: Meal;
+  constructor() {
+    this.onMealSelect = new EventEmitter();
+  }
+  mealClicked(clickedMeal: Meal): void {
+    this.selectedMeal = clickedMeal;
+    this.onMealSelect.emit(clickedMeal);
+  }
+}
+
 @Component({
   selector: 'my-app',
+  directives: [MealListComponent]
   template: `
     <div class="container">
       <h1>Mega Meal Tracker</h1>
+      <meal-list
+      [mealList]="meals"
+      (onMealSelect)="mealWasSelected($event)">
+      </meal-list>
     </div>
   `
 })
@@ -28,12 +46,12 @@ export class AppComponent {
   public meals: Meal[];
   constructor(){
     this.meals = [
-      new Meal("Toast and OJ", "Didn't eat the crust. I hate crust.", 85),
-      new Meal("Big Mac and Soda", "The soda was diet Coke!", 541)
+      new Meal("Toast and OJ", "Didn't eat the crust. I hate crust.", 85, 0),
+      new Meal("Big Mac and Soda", "The soda was diet Coke!", 541, 1)
     ];
   }
   mealWasSelected(clickedMeal: Meal): void {
-    console.log(clickedMeal); //take this out later.
+    console.log(meals, clickedMeal); //take this out later.
   }
 }
 
